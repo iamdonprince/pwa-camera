9daf5fabdf663a7be0fcde0a425ff25bc653cdb1
iamdonprince <princekumax@gmail.com>
2021-08-24 10:09:25 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,62 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import Webcam from "react-webcam";
+import "./App.css";
 
 function App() {
+  const [images, setImages] = React.useState([]);
+  const videoConstraints = {
+    facingMode: "user",
+  };
+
+  const webcamRef = React.useRef(null);
+
+  const takePhoto = React.useCallback(() => {
+    const imageSrc = webcamRef.current.getScreenshot();
+    setImages([...images, imageSrc]);
+    console.log(imageSrc);
+  }, [webcamRef, images]);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
+      <nav id="navbar">Pwa Camera</nav>
+      <main
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+          alignItems: "space-between",
+        }}
+      >
+        <Webcam
+          audio={false}
+          height="20%"
+          ref={webcamRef}
+          screenshotFormat="image/jpeg"
+          width="100%"
+          videoConstraints={videoConstraints}
+        />
+        <div
+          style={{
+            width: "100%",
+            padding: "10px",
+            display: "flex",
+            flexWrap: "wrap",
+          }}
+          classNam="imagesContainer"
         >
-          Learn React
-        </a>
-      </header>
+          {images.length &&
+            images.map((image) => (
+              <img
+                src={image}
+                style={{ width: "100px", height: "100px", margin: "10px" }}
+                alt="clicked images"
+              />
+            ))}
+        </div>
+        <button id="camera-btn" onClick={takePhoto}>
+          Take Photo
+        </button>
+      </main>
     </div>
   );
 }
